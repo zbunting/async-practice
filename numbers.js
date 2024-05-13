85495c730f3cbad009fbd01bd3249daa7efb9e42
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-13 19:39:48 -0400

    Code for showNumberRace function

--- numbers.js
+++ numbers.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const NUMBERS_API_BASE = "http://numbersapi.com/";
 
 function showNumberTrivia(num) {
@@ -7,5 +9,23 @@ function showNumberTrivia(num) {
         })
         .then(numberData => { // runs on the resp.json() promise
             console.log(numberData.text); //numberData should be what we get back from API
-        })
+        });
+}
+
+
+/** Given four numbers, returns piece of trivia for whichever response gets
+ * returned quickest by the API. */
+function showNumberRace(num1, num2, num3, num4) {
+    const p1 = fetch(`${NUMBERS_API_BASE}${num1}?json`);
+    const p2 = fetch(`${NUMBERS_API_BASE}${num2}?json`);
+    const p3 = fetch(`${NUMBERS_API_BASE}${num3}?json`);
+    const p4 = fetch(`${NUMBERS_API_BASE}${num4}?json`);
+
+    const answerPromise = Promise.race(
+        [p1, p2, p3, p4]
+    );
+
+    answerPromise
+        .then(resp => resp.json())
+        .then(winner => console.log(winner.text));
 }
